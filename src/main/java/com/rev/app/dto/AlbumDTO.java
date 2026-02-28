d43f5af85d2d20a617b61d4747d68ad97c49b29a
DevBaburao <baburaobure0@gmail.com>
2026-02-28 15:30:28 +0530

    Created AlbumDTO for safe data transfer between controller and service

---
+++ src/main/java/com/rev/app/dto/AlbumDTO.java
@@ -0,0 +1,70 @@
+package com.rev.app.dto;
+
+import java.time.LocalDate;
+
+public class AlbumDTO {
+
+    private Long id;
+    private Long artistId;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private String coverImage;
+    private Integer isDeleted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(Long artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
